1b426f2e207ed7b3426d7e392b789b40800c7c0b
youngwunko <hiaro711@gmail.com>
2020-10-04 13:53:14 +0900

    Create opencv.h
    opencv header

---
+++ opencv.h
@@ -0,0 +1,8 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/saliency.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/ml.hpp>
+
+using namespace cv;
+using namespace saliency;
+using namespace cv::ml;
